dc1c72f31b342c4f9521918695e886d5f0334e29
anirudh-tiwari <anirudht2001@gmail.com>
2024-03-25 21:40:33 +0530

    add: types array

--- app.ts
+++ app.ts
@@ -1,2 +1,15 @@
-let a:number = 9;
-console.log('Hello Typescript');
+let data = ["anirudh", "23"];
+data.push(9);  //number can't go to array of strings
+data.push("9"); // No issue
+
+let data2 = ["anirudh", "23",9];
+data2.push(9);  //now it is valid. 
+data2.push(true);   //boolean can't go to array of strings & number
+
+let data3 = ["anirudh", "23"];
+data3[2] = true;  // Type 'boolean' is not assignable to type 'string'.
+data3[2] = "true";  // Type 'boolean' is not assignable to type 'string'.
+
+
+let data4: string[]  = ["anirudh", "23"];
+data3[2] = "true";
