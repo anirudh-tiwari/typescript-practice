1dbde44f7880001410b62bc6629bc7025879997c
anirudh-tiwari <anirudht2001@gmail.com>
2024-03-26 22:57:50 +0530

    add: type object

--- app.ts
+++ app.ts
@@ -1,15 +1,15 @@
-let data = ["anirudh", "23"];
-data.push(9);  //number can't go to array of strings
-data.push("9"); // No issue
+let data = {
+    'name': 'anirudh',
+    'age': 12,
+}
 
-let data2 = ["anirudh", "23",9];
-data2.push(9);  //now it is valid. 
-data2.push(true);   //boolean can't go to array of strings & number
+// data.name = 10;     ERROR
+// data.name = "10";   CORRECT
+// data.EMAIL = "ani@gmail";  Error ==> We can't add data in typescript
 
-let data3 = ["anirudh", "23"];
-data3[2] = true;  // Type 'boolean' is not assignable to type 'string'.
-data3[2] = "true";  // Type 'boolean' is not assignable to type 'string'.
+let data2:any = {
+    'name': 'anirudh',
+    'age': 12,
+}
 
-
-let data4: string[]  = ["anirudh", "23"];
-data3[2] = "true";
+// data2.name = true;  Correct 
