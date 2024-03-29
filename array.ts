682a9d2f40974e4a285e03f31cbeb27f2a981728
anirudh-tiwari <anirudht2001@gmail.com>
2024-03-29 23:20:21 +0530

    add: type array complete

---
+++ array.ts
@@ -0,0 +1,10 @@
+// const data: string[] = ['ANirudh','23','10c'];
+
+// const data: any[] = ['ANirudh',23,'10c'];
+
+// const data:[string, number, string] = ['ANirudh',23,'10c'];  // NOt recommended because we don't know how many values can come to aaray
+
+// const data = ['ANirudh',23,'10c'];  //Recommeneded when string length is long . DOn't pass any type
+
+// data.push('124')  // Correct
+// data.push(true)  // InCorrect
