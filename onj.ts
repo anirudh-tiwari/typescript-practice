c1acb4b0f01b9bc57bf988d0219364a1910e4858
anirudh-tiwari <anirudht2001@gmail.com>
2024-03-29 23:14:25 +0530

    add: Object type all cases

---
+++ onj.ts
@@ -0,0 +1,34 @@
+// 1 WAY 
+// const person:{name: string, age: number } = {
+//     'name': 'ANirudh',
+//     'age': 20,
+// }
+
+// console.log(person.name)
+
+// 2 WAY 
+// type ObjType = {name: string, age: number }
+// const person:ObjType = {
+//     'name': 'ANirudh',
+//     'age': 20,
+// }
+
+// console.log(person.name)
+
+// 3 WAY 
+// let data = {
+//     'name': 'anirudh',
+//     'age': 12,
+// }
+
+// data.name = 10;     ERROR
+// data.name = "10";   CORRECT
+// data.EMAIL = "ani@gmail";  Error ==> We can't add data in typescript
+
+// 4 WAY 
+// let data2:any = {
+//     'name': 'anirudh',
+//     'age': 12,
+// }
+
+// data2.name = true;  Correct 
